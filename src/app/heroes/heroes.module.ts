7559c111f09ab6e1749c4d17cca46aeef76a4cbe
PavelOsw <pavel.osw117@gmail.com>
2023-10-22 22:11:24 -0600

    [WIP] reactive form for add or create hero

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -5,12 +5,14 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from '../material/material.module';
 import { HeroesRoutingModule } from './heroes-routing.module';
 
+import { CardComponent } from './components/card/card.component';
+import { ConfirmDialogComponent } from './components/confirm-dialog/confirm-dialog.component';
 import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
-import { CardComponent } from './components/card/card.component';
+
 import { HeroImagePipe } from './pipes/hero-image.pipe';
 
 
@@ -22,6 +24,7 @@ import { HeroImagePipe } from './pipes/hero-image.pipe';
     NewPageComponent,
     SearchPageComponent,
     CardComponent,
+    ConfirmDialogComponent,
 
     // Pipes
     HeroImagePipe,
